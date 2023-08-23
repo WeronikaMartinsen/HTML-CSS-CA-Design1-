e13cb963502d9ce98536d9b1752454dbf749aa80
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-08-23 12:49:24 +0200

    working on functions

--- js/script.js
+++ js/script.js
@@ -25,42 +25,52 @@ async function displayJackets() {
   for (let i = 0; i < jackets.length; i++) {
     const jacket = jackets[i];
     console.log(jacket);
+    const price = jacket.price;
 
-    if (jacket.onSale) {
-      const jacketDiv = document.createElement("div");
-      jacketDiv.classList.add("card");
-      jacketsContainer.appendChild(jacketDiv);
+    /*     const onSale = jacket.onSale;
+  
+   
+    const discountedPrice = jacket.discountedPrice;
+ 
 
-      const image = document.createElement("img");
-      image.src = jacket.image;
-      image.alt = jacket.description;
-      image.classList.add("image");
+    if(onSale === true) {
+      discountedPrice
+    }
+ */
 
-      const jacketTitle = document.createElement("jacketTitle");
-      jacketTitle.classList.add("jacketTitle");
-      jacketTitle.innerHTML = `<h4>${jacket.title}</h4>`;
+    const jacketDiv = document.createElement("div");
+    jacketDiv.classList.add("card");
+    jacketsContainer.appendChild(jacketDiv);
 
-      const jacketText = document.createElement("jacketText");
-      jacketText.classList.add("jacketText");
-      jacketText.innerHTML = `<h5>${jacket.description}</h5>
+    const image = document.createElement("img");
+    image.src = jacket.image;
+    image.alt = jacket.description;
+    image.classList.add("image");
+
+    const jacketTitle = document.createElement("jacketTitle");
+    jacketTitle.classList.add("jacketTitle");
+    jacketTitle.innerHTML = `<h4>${jacket.title}</h4>`;
+
+    const jacketText = document.createElement("jacketText");
+    jacketText.classList.add("jacketText");
+    jacketText.innerHTML = `<h5>${jacket.description}</h5>
      `;
 
-      const jacketPrice = document.createElement("jacketPrice");
-      jacketPrice.classList.add("jacketPrice");
-      jacketPrice.innerHTML = ` <span class="jacketSale">${jacket.price}</span>
-     <span>${jacket.discountedPrice}</span>`;
+    const jacketPrice = document.createElement("jacketPrice");
+    jacketPrice.classList.add("jacketPrice");
+    jacketPrice.innerHTML = ` <span class="jacketSale">${price}</span>
+     <span class="newPrice">${jacket.discountedPrice}</span>`;
 
-      const button = document.createElement("a");
-      button.href = "product-details.html";
-      button.classList.add("btnAdd");
-      button.textContent = "Add to bag";
+    const button = document.createElement("a");
+    button.href = "product-details.html";
+    button.classList.add("btnAdd");
+    button.textContent = "Add to bag";
 
-      jacketDiv.appendChild(jacketTitle);
-      jacketDiv.appendChild(image);
-      jacketDiv.appendChild(jacketText);
-      jacketDiv.appendChild(jacketPrice);
-      jacketDiv.appendChild(button);
-    }
+    jacketDiv.appendChild(jacketTitle);
+    jacketDiv.appendChild(image);
+    jacketDiv.appendChild(jacketText);
+    jacketDiv.appendChild(jacketPrice);
+    jacketDiv.appendChild(button);
   }
 }
 
