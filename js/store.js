a85169c7c624988016ed1d88efc542930e8a3961
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-09-04 17:07:41 +0200

    working on product checkout

--- js/store.js
+++ js/store.js
@@ -1,11 +1,11 @@
 const removeCardButtons = document.getElementsByClassName("btnRemove");
 console.log(removeCardButtons);
 
-for (i = 0; i < removeCardButtons.length; i++) {
+for (let i = 0; i < removeCardButtons.length; i++) {
   allRemoveButtons = removeCardButtons[i];
   allRemoveButtons.addEventListener("click", function (event) {
     const buttonClicked = event.target;
-    buttonClicked.parentElement.parentElement.parentElement.remove();
+    buttonClicked.parentElement.parentElement.remove();
     updateCartTotal();
   });
 }
@@ -13,12 +13,13 @@ for (i = 0; i < removeCardButtons.length; i++) {
 function updateCartTotal() {
   const cartItemContainer = document.getElementsByClassName("checkoutDiv")[0];
   const cartRows = cartItemContainer.getElementsByClassName("cart-row");
-  for (i = 0; i < cartRows.length; i++) {
+  for (let i = 0; i < cartRows.length; i++) {
     const cartRow = cartRows[i];
     const cartPrice = cartRow.getElementsByClassName("cartPrice")[0];
     const quantityElement = cartRow.getElementsByClassName("quantityInput")[0];
     const price = parseFloat(cartPrice.innerText.replace("$", ""));
     const quantity = quantityElement.value;
+    console.log(price);
     console.log(price * quantity);
   }
 }
