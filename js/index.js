3089677e6c386c1cd6f43d47dfddfbe330007ef0
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-09-07 21:40:09 +0200

    working on styles

--- js/index.js
+++ js/index.js
@@ -35,8 +35,8 @@ async function fetchJackets() {
             jacketPrice.innerHTML = `<span class="normalPrice">${price} ,-</span>`;
           } else {
             jacketPrice.innerHTML = `
-            <span class="oldPrice">${price} ,-</span>
-            <span class="jacketSale">${sale} ,-</span>
+            <span class="oldPrice">Before: ${price} ,-</span>
+            <span class="jacketSale"> ${sale} ,-</span>
             `;
           }
 
